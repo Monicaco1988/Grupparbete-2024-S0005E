c0d5ca6e459f7d7837b24a9199067fe8a7310dbb
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2024-02-09 17:23:26 +0100

    Implemented some buildnings

--- Scripts/CameraFollow.cs
+++ Scripts/CameraFollow.cs
@@ -6,12 +6,13 @@ public partial class CameraFollow : Node3D
 {
 
 
-	// The CollisionShape3D node to check for collisions
-	 [Export]
-	 public CollisionShape3D checkpoint;
+	private int leadingCarId = 0;
 
-	// [Export]
-	// public Area3D[] checkpointArea; // array of Areas?
+	private int collisionShapes = 0;
+
+	// The CollisionShape3D node to check for collisions
+	[Export]
+	public Area3D checkpoint;
 
 	// imports the attributes of the player
 	[Export]
@@ -19,52 +20,36 @@ public partial class CameraFollow : Node3D
 
 	// makes the camera follow the target faster och slower
 	[Export]
-	float cameraSmoothing = 10.0f;
+	float cameraSmoothing = 5.0f;
 
 	//Takes the node of the camera
 	[Export]
 	Node3D cameraRoot;
 	public override void _Ready()
 	{
-		//sets the camera pivot to the leading car position
-		this.GlobalPosition = leadingCar.GlobalPosition;
+		//sets the camera pivot to the leading car position (the id has to be set later but for now it should find the index 0,1,2,3,4,etc)
+		this.GlobalPosition = leadingCar.GetChild<RigidBody3D>(0).GlobalPosition;
 	}
 
-public void OnPlayerEnter(Node3D playerContainer)
-{
-	GD.Print("Collision detected with: " + playerContainer.Name);
-}
+	public void OnPlayerEnter(Node3D playerContainer)
+	{
+		GD.Print("Collision detected with: " + playerContainer.Name);
 
-public void OnPlayerExit(Node3D playerContainer)
-{	
-GD.Print("Collision exited with: " + playerContainer,Name);
-}
+		if (playerContainer.Name == leadingCar.GetChild<RigidBody3D>(1).Name)
+		{
+			leadingCarId = 1;
+		}
+		else if (playerContainer.Name == leadingCar.GetChild<RigidBody3D>(0).Name)
+		{
+			leadingCarId = 0;
+		}
+		checkpoint.GetChild<CollisionShape3D>(collisionShapes).QueueFree();
+	}
 
-	// private void Area_AreaEntered(Area3D area)
-	// {
-	// 	leadingCar.Position = area.Position;
-	// 	//MoveCameraHere
-	// }
 
 	public override void _PhysicsProcess(double delta)
 	{
-		cameraRoot.GlobalPosition = cameraRoot.GlobalPosition.Lerp(leadingCar.GlobalPosition, (float)delta * cameraSmoothing);
-
-		// foreach (checkpoint in checkpointArea) // do i have to make checkpoints as array?
-		// {
-		// 	checkpointArea.AreaEntered += Area_AreaEntered;
-		// }
-
-		//GD.Print(checkpoint.Position.X);
-
-
-		//foreach (checkpoint in root as area)
-		//      {S
-		//          area.AreaEntered += Area_AreaEntered;
-		//      }
-		//LookAt(leadingCar.Position, Godot.Vector3.Up);
-
-		//GetNode<RigidBody3D>("/root/World/Player").LookAt(Position, Godot.Vector3.Up);
+		this.GlobalPosition = this.GlobalPosition.Lerp(leadingCar.GetChild<RigidBody3D>(leadingCarId).GlobalPosition, (float)delta * cameraSmoothing);
 	}
 }
 
