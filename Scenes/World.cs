b34f88eb431852563af5542360c4d20b01c040dd
Cecilia <cecilia.gradin3@gmail.com>
2024-02-14 08:11:01 +0100

    simple main menu

---
+++ Scenes/World.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class World : Node3D
+{
+	public override void _UnhandledInput(InputEvent @event){
+		if(@event.IsActionPressed("startGame")){
+			GetNode<Control>("MainMenu/menuBackground").Hide();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
